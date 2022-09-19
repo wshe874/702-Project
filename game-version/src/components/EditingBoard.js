765b1755b311177efcff2beec789e68ddf0af2e0
alyu201 <alyu201@aucklanduni.ac.nz>
2022-09-19 15:11:39 +1200

    Add component for the body of the level page

---
+++ game-version/src/components/EditingBoard.js
@@ -0,0 +1,7 @@
+import Box from '@mui/material/Box';
+
+const EditingBoard = () => {
+  return <Box sx={{ width: '100%', height: '100%', p: 1 }}>Put content in EditingBoard component for the level</Box>;
+};
+
+export default EditingBoard;
