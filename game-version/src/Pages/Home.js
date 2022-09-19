804ca4f796dee94ccc3b1dd84772d66ac7a053df
MeganLim97 <mlim807@aucklanduni.ac.nz>
2022-09-19 23:49:49 +1200

    Finished the basic layout of the Landing page.

--- game-version/src/Pages/Home.js
+++ game-version/src/Pages/Home.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Grid from '@mui/material/Grid';
+import { Box, Button } from "@mui/material";
 
 function Home() {
-  return <div>Welcome</div>;
+
+  return(
+  <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignContent: 'stretch',
+          p: 2,
+          height: '87vh',
+        }}
+      >
+        <Grid container spacing={2} justifyContent="space-around">
+          <Grid container
+              direction="column" justifyContent='center' alignItems='center' item xs={6}>
+            <h1>Game of Fitts'</h1>
+            <Button sx={{color: 'black', backgroundColor: '#b3db59'}} variant="outlined" size="large" disableRipple>Start</Button>
+          </Grid>
+          <Grid container direction="column" justifyContent='center' alignItems='center' item xs={6}>
+            <p>plant</p>
+          </Grid>
+        </Grid>
+      </Box>
+  </>
+    
+  );
 }
 
 export default Home;
