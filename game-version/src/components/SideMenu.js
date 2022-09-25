3b1f14000731030b25756f6c2e6b755ad36e49be
MeganLim97 <mlim807@aucklanduni.ac.nz>
2022-09-25 14:23:11 +1300

    Made the Ready button and the column headers in the side bar.

--- game-version/src/components/SideMenu.js
+++ game-version/src/components/SideMenu.js
@@ -1,8 +1,8 @@
 import { useContext, useEffect, useState } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import { Card, CardContent, Button, Grid, Item } from '@mui/material';
 import Animation from '../components/Animation';
 import { GameLogicContext } from '../contexts/GameLogicContextProvider';
+import SideMenuTable from './sideMenuComponents/SideMenuTable'
 
 
 const SideMenu = () => {
@@ -14,11 +14,12 @@ const SideMenu = () => {
   },[plantStage, setPlantStage, setLocalPlantStage, localPlantStage]);
 
 
-
   return (
     <Card sx={{ width: '100%', height: '100%', background: '#f4f4f4', boxShadow: 'none', borderRadius: '1rem' }}>
-      <CardContent>Put content in SideMenu component for the measurements and plant
-      <Animation stage={localPlantStage}></Animation>
+      <CardContent>
+        <Button sx={{color: 'black', backgroundColor: '#b3db59', marginTop: '30px', marginBottom: '40px'}} variant="outlined" size="large" disableRipple>Ready</Button>
+        <SideMenuTable/>
+        <Animation stage={localPlantStage}></Animation>
       </CardContent>
       
     </Card>
