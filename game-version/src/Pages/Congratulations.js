58ed1c3b2f8b7c7b5b7809ff7e68709f95d765e6
Lance Delos Reyes <lan_drey19@hotmail.com>
2022-09-27 02:25:16 +1300

    use real attempt data instead of placeholder data

--- game-version/src/Pages/Congratulations.js
+++ game-version/src/Pages/Congratulations.js
@@ -4,7 +4,6 @@ import Animation from "../components/Animation";
 import BarChart from "../components/BarChart";
 import { Box, Button, Card, CardContent, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
-import { placeholderGameData } from "../utils/placeholderGameData";
 import { GameLogicContext } from "../contexts/GameLogicContextProvider";
 
 function Congratulations() {
@@ -14,42 +13,48 @@ function Congratulations() {
     },
   };
 
-  const { plantStage } = useContext(GameLogicContext);
-  const [gameData, setGameDate] = useState({
-    labels: ["Attempt 1", "Attempt 2", "Attempt 3"],
+  let AttemptNumber = 0;
+  const { plantStage, gameResults } = useContext(GameLogicContext);
+  const finalLabels = gameResults.map((result) => {
+    AttemptNumber++;
+    return "Attempt " + AttemptNumber;
+  });
+
+  const gameData = {
+    labels: finalLabels,
     datasets: [
       {
         label: "Prompt 1",
-        data: placeholderGameData.map((data) => data.promptTimes[0]),
+        data: gameResults.map((data) => data[0].averageMovementTime),
         backgroundColor: ["#AF81C9"],
       },
       {
         label: "Prompt 2",
-        data: placeholderGameData.map((data) => data.promptTimes[1]),
+        data: gameResults.map((data) => data[1].averageMovementTime),
         backgroundColor: ["#F89A7E"],
       },
       {
         label: "Prompt 3",
-        data: placeholderGameData.map((data) => data.promptTimes[2]),
+        data: gameResults.map((data) => data[2].averageMovementTime),
         backgroundColor: ["#F2CA85"],
       },
       {
         label: "Prompt 4",
-        data: placeholderGameData.map((data) => data.promptTimes[3]),
+        data: gameResults.map((data) => data[3].averageMovementTime),
         backgroundColor: ["#54D1F1"],
       },
       {
         label: "Prompt 5",
-        data: placeholderGameData.map((data) => data.promptTimes[4]),
+        data: gameResults.map((data) => data[4].averageMovementTime),
         backgroundColor: ["#7C71AD"],
       },
       {
         label: "Prompt 6",
-        data: placeholderGameData.map((data) => data.promptTimes[5]),
+        data: gameResults.map((data) => data[5].averageMovementTime),
         backgroundColor: ["#445569"],
       },
     ],
-  });
+  };
   return (
     <>
       <Box
@@ -86,11 +91,11 @@ function Congratulations() {
             <Card style={{ border: "2px solid #b3db59" }}>
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                  Your best result!
+                  See your progress here!
                 </Typography>
                 <br></br>
                 <Typography variant="body2" color="text.secondary">
-                  Average time in milliseconds (ms) for each successful attempt
+                  Average time in milliseconds (ms) for each attempt
                 </Typography>
                 <br></br>
                 <div style={{ height: "380px", width: "700px" }}>
