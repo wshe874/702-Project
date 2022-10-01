1d953bb611ecf9a5e071d375a2741de58644cb40
MeganLim97 <mlim807@aucklanduni.ac.nz>
2022-10-02 00:06:13 +1300

    Added the hover text for the MT column in the side-bar table's header.

--- game-version/src/components/sideMenuComponents/SideMenuHeader.js
+++ game-version/src/components/sideMenuComponents/SideMenuHeader.js
@@ -1,4 +1,4 @@
-import { Grid, Paper } from '@mui/material';
+import { Grid, Paper, Tooltip } from '@mui/material';
 
 
 const SideMenuHeader = () => {
@@ -26,7 +26,9 @@ const SideMenuHeader = () => {
           <Paper elevation={0} style={cell}>Distance</Paper>
         </Grid>
         <Grid item xs={2}>
-          <Paper elevation={0} style={cell}>Average movement time (ms)</Paper>
+          <Tooltip title={<h4 style={{margin: '0'}}>MT is the time taken to complete the clicking task</h4>} placement="top-end" arrow>
+            <Paper elevation={0} style={cell}>Average movement time (ms)</Paper>
+          </Tooltip>
         </Grid>
       </>
   );
