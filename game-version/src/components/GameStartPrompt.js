b9dfbb9c6dbf92afd90dd7f58c21d8db87bedca4
alyu201 <alyu201@aucklanduni.ac.nz>
2022-10-02 19:55:10 +1300

    Better formatting of text and fixed formula

--- game-version/src/components/GameStartPrompt.js
+++ game-version/src/components/GameStartPrompt.js
@@ -46,16 +46,18 @@ const GameStartPrompt = () => {
 
             <p style={{ backgroundColor: 'rgb(179, 219, 89, 50%)', padding: '8px 24px', borderRadius: '2rem' }}>
               <i>
-                <b>MT = a + b log_2(D/W)</b>
+                <b>MT = a + b log_2(D/W + 1)</b>
               </i>
             </p>
 
             <p>
               where <b>a</b> and <b>b</b> are coefficients determined through experiments so you do not have to worry
               about these.
-              <br />
-              The key takeaway here is that the{' '}
-              <b>shorter the distance and bigger the target size, the faster is it to perform clicking tasks</b>.
+            </p>
+
+            <p>
+              The key takeaway here is that the <b>the distance and the target size will determine the movement time</b>
+              .
             </p>
           </Box>
           <Box
@@ -99,8 +101,11 @@ const GameStartPrompt = () => {
               An interface will be given for you to improve by dragging and resizing buttons for better interactions.
               With the redesigned interface, press the <b>‘Ready’</b> button to start the clicking tasks. You will be
               prompted to complete <b>6 clicking tasks</b>. The faster you complete the clicking tasks, the more the
-              plant will grow. <b>3 attempts</b> will be given. If the movement time has not improved after the 3rd
-              attempt, the plant will die.
+              plant will grow.
+            </p>
+            <p>
+              <b>3 attempts</b> will be given. If the movement time has not improved after the 3rd attempt, the plant
+              will die.
             </p>
             <p>
               Click <b>‘Start’</b> to begin designing the interface.
